1e0c207da97b39d5b617f8d7057ed8a326b42b10
Mori <64525549+angelidito@users.noreply.github.com>
2022-06-23 18:13:30 +0200

    Create set_header.sh

---
+++ set_header.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# ./set_header.sh
+
+# Set variables
+
+if [ ! -z "$USER" ]
+then
+    echo "USER=`/usr/bin/angel`" >> ~/.zshrc
+    echo "export USER" >> ~/.zshrc
+fi
+
+if [ ! -z "$GROUP" ]
+then
+    echo "GROUP=`/usr/bin/id -gn $user`" >> ~/.zshrc
+    echo "export GROUP" >> ~/.zshrc
+fi
+
+if [ ! -z "$MAIL" ]
+then
+    echo "MAIL="$angmarti@student.42madrid.com"" >> ~/.zshrc
+    echo "export MAIL" >> ~/.zshrc
+fi
+
+mkdir -p ~/.vim/plugin
+
+# Add stdheader to vim plugins
+cp plugin/stdheader.vim ~/.vim/plugin/
+
+source ~/.zshrc
