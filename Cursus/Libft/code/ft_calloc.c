e46cb2caa8ecb6c891f672128232f29c2abf0da7
Mori from 42 <64525549+angelidito@users.noreply.github.com>
2022-07-05 16:57:20 +0200

    Strdup: OK; Calloc: ¿OK?

--- Cursus/Libft/code/ft_calloc.c
+++ Cursus/Libft/code/ft_calloc.c
@@ -6,13 +6,19 @@
 /*   By: angmarti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/30 18:58:20 by angmarti          #+#    #+#             */
-/*   Updated: 2022/06/30 20:14:33 by angmarti         ###   ########.fr       */
+/*   Updated: 2022/07/05 16:31:20 by angmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-/*
+
 void	*ft_calloc(size_t count, size_t size)
 {
+	void	*ptr;
 
-}*/
+	ptr = malloc(count * size);
+	if (!ptr || ptr == NULL)
+		return (NULL);
+	ft_bzero(ptr, count * size);
+	return (ptr);
+}
