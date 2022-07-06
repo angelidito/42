5ed973a77f5f293025f0465e2e09722654575a7f
Mori from 42 <64525549+angelidito@users.noreply.github.com>
2022-07-06 13:27:56 +0200

    Substr OK

--- Cursus/Libft/code/ft_substr.c
+++ Cursus/Libft/code/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: angmarti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/05 17:08:44 by angmarti          #+#    #+#             */
-/*   Updated: 2022/07/05 17:39:33 by angmarti         ###   ########.fr       */
+/*   Updated: 2022/07/06 13:21:33 by angmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,21 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*sub;
 
+	if (!s)
+		return (NULL);
+	if (ft_strlen(s) < (size_t)start)
+	{
+		sub = malloc(1);
+		if (!sub)
+			return (NULL);
+		*sub = '\0';
+		return (sub);
+	}
+	if (ft_strlen(s) - start - 1 < len)
+		len = ft_strlen(s) - start;
 	sub = malloc(len + 1);
-	ft_strlcpy(sub, s + start, len +1);
+	if (!sub)
+		return (NULL);
+	ft_strlcpy(sub, s + start, len + 1);
 	return (sub);
 }
