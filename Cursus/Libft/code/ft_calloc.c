56783863d8ad8c8cef2ba4e11ee71897efc57533
Mori from 42 <64525549+angelidito@users.noreply.github.com>
2022-07-13 11:35:12 +0200

    Update ft_calloc.c
    I think it's more ok now

--- Cursus/Libft/code/ft_calloc.c
+++ Cursus/Libft/code/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: angmarti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/30 18:58:20 by angmarti          #+#    #+#             */
-/*   Updated: 2022/07/05 16:31:20 by angmarti         ###   ########.fr       */
+/*   Updated: 2022/07/13 11:33:29 by angmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,14 @@
 void	*ft_calloc(size_t count, size_t size)
 {
 	void	*ptr;
+	size_t	len;
 
-	ptr = malloc(count * size);
+	len = count * size;
+	if (!len)
+		len = 1;
+	ptr = malloc(len);
 	if (!ptr || ptr == NULL)
 		return (NULL);
-	ft_bzero(ptr, count * size);
+	ft_bzero(ptr, len);
 	return (ptr);
 }
