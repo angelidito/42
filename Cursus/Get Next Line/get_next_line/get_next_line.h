fd5832560a03890a7e85342650459ef7d77d1fa8
Mori from 42 <64525549+angelidito@users.noreply.github.com>
2022-08-21 15:28:41 +0200

    Update get_next_line.h

--- Cursus/Get Next Line/get_next_line/get_next_line.h
+++ Cursus/Get Next Line/get_next_line/get_next_line.h
@@ -1,20 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: angmarti <angmarti@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/16 17:04:29 by angmarti          #+#    #+#             */
+/*   Updated: 2022/08/17 14:12:22 by angmarti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// This is a header guard. It prevents the header from being included twice.
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+// These are the libraries that are needed for the program to work.
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdio.h>
 # include <fcntl.h>
 # include <limits.h>
 
+// This is a preprocessor directive. 
+// It checks if the macro BUFFER_SIZE is not defined. 
+// If it is not defined, it defines it as 1.
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 5
+#  define BUFFER_SIZE 1
 # endif
 
-int		freecat(char **oldline, char *buff, ssize_t bytes);
-int		read_until_newline(int fd, char **line);
-char	*get_until_newline(char **oldline);
+
+// These are the function prototypes. 
+// They tell the compiler what the function will return, 
+// what the function will be called, and what parameters the function will take.
+
+ssize_t	gnl_read_and_add(int fd, char **currentline, size_t len);
+char	*gnl_cut_line(char **currentline, size_t len);
 char	*get_next_line(int fd);
+
+// Prototypes of the first version
+// int		freecat(char **oldline, char *buff, ssize_t bytes);
+// int		read_until_newline(int fd, char **line);
+// char	*get_until_newline(char **oldline);
 
 //Utils
 /*
@@ -24,4 +50,5 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 char	*ft_strjoin(char const *s1, char const *s2);
 */
 
+// The end of the header guard.
 #endif
