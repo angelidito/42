0e10a21130e1984be772c2c89bbfc045baead7c1
Mori from 42 <64525549+angelidito@users.noreply.github.com>
2023-02-01 16:57:19 +0100

    Update render_next_frame.c

--- Cursus/Circle2/FdF/src/events/render_next_frame.c
+++ Cursus/Circle2/FdF/src/events/render_next_frame.c
@@ -6,7 +6,7 @@
 /*   By: angmarti <angmarti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 11:37:20 by angmarti          #+#    #+#             */
-/*   Updated: 2023/02/01 15:00:53 by angmarti         ###   ########.fr       */
+/*   Updated: 2023/02/01 16:51:13 by angmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@
  * of the image, and then puts the image to the window.
  * 
  * Include this for more info:
- * 	ft_printf("abs_scale : %f\n", vars->map->abs_scale);
- *	ft_printf("  z_scale : %f\n", vars->map->z_scale);
- *	ft_printf("    scale : %f\n", vars->map->scale);
- *	ft_printf("    angle : %f\n", vars->map->angle);
- *	ft_printf("     desv : %f\n", vars->map->desv);
+ * 	printf("abs_scale : %f\n", vars->map->abs_scale);
+ *	printf("  z_scale : %f\n", vars->map->z_scale);
+ *	printf("    scale : %f\n", vars->map->scale);
+ *	printf("    angle : %f\n", vars->map->angle);
+ *	printf("     desv : %f\n", vars->map->desv);
  *
  * @param vars a pointer to the t_vars structure
  * 
