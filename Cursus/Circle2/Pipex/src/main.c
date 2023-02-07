d29f0f1bb59de777140ecdbe9f1fec6db64386bb
Mori from 42 <64525549+angelidito@users.noreply.github.com>
2023-02-07 16:24:16 +0100

    pruebas

--- Cursus/Circle2/Pipex/src/main.c
+++ Cursus/Circle2/Pipex/src/main.c
@@ -6,11 +6,16 @@
 /*   By: angmarti <angmarti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 17:22:16 by angmarti          #+#    #+#             */
-/*   Updated: 2023/02/06 19:48:29 by angmarti         ###   ########.fr       */
+/*   Updated: 2023/02/07 15:55:13 by angmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incs/pipex.h"
+#include <fcntl.h> /* Open function */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
 
 void	breakpoint(void)
 {
@@ -19,64 +24,68 @@ void	breakpoint(void)
 
 void	child(int *pipe_fd)
 {
-	ft_printf("Child ft\n");
+	char	buffer[512];
+	pid_t	pid;
+
+	pid = fork();
+	close(pipe_fd[1]); /* cerramos el lado de escritura del pipe */
+	while ((read(pipe_fd[0], buffer, 1)) > 0)
+		write(1, buffer, 1);
 	close(pipe_fd[0]);
-	breakpoint();
-	if (write(pipe_fd[1], "MEMENTO MORI", 12) == -1)
+	if (pid)
 	{
-		ft_printf("Error while writing at fd %d.\n", pipe_fd[1]);
-		exit(1);
-	}
-	breakpoint();
-		ft_printf("\n");
-	close(pipe_fd[1]);
-}
-void	parent(int *pipe_fd)
-{
-	char	buff[100];
-
-	ft_printf("parent ft\n");
-	wait(NULL);
-	close(pipe_fd[1]);
-	if (read(pipe_fd[0], buff, 12) == -1)
-	{
-		ft_printf("Error while writing at fd %d.\n", pipe_fd[1]);
-		exit(1);
-	}
-	close(pipe_fd[0]);
-	ft_printf("-_  %s\n", buff);
-}
-
-int	main(void)
-{
-	int		id;
-	int		i;
-	char	*str;
-
-	int pipe_fd[2]; // 0 - read ; 1 - write
-	str = "hola";
-	i = 0;
-	id = fork();
-	if (pipe(pipe_fd) == -1)
-	{
-		perror("Error while piping.");
-		exit(1);
-	}
-	wait(NULL);
-	// i = 69;
-	// close(pipe_fd[0]);
-	// write(pipe_fd[1], str, sizeof(char) * ft_strlen(str));
-	// close(pipe_fd[1]);
-	// read(pipe_fd[0], buff, 12);
-	// close(pipe_fd[0]);
-	// ft_printf("%s\n", buff);
-	if (id == 0)
-	{
-		child(pipe_fd);
+		waitpid(0, NULL, 0);
+		ft_printf("Termina el hijo.\n");
 	}
 	else
 	{
-		parent(pipe_fd);
+		ft_printf("Termina el nieto.\n");
 	}
+}
+
+void	parent(int *pipe_fd)
+{
+	char	buffer[512];
+
+	close(pipe_fd[0]); /* cerramos el lado de lectura del pipe */
+	ft_strlcpy(buffer, "Esto llega a traves de la tuberia\n", 35);
+	write(pipe_fd[1], buffer, ft_strlen(buffer));
+	close(pipe_fd[1]);
+	waitpid(0, NULL, 0);
+	ft_printf("Termina el padre.\n");
+}
+
+int	main(int argc, char const *argv[])
+{
+	int		i;
+	char	*str;
+	pid_t	pid;
+
+	if (argc < 2)
+	{
+		fprintf(stderr, "Usage: %s file1 command1 command1 file2\n", argv[0]);
+		// exit(EXIT_FAILURE);
+	}
+	ft_printf("Comienza el Programa.\n");
+	int pipe_fd[2]; // 0 - read ; 1 - write
+	str = "hola";
+	i = 0;
+	if (pipe(pipe_fd) == -1)
+	{
+		perror("Error while piping.");
+		exit(EXIT_FAILURE);
+	}
+	// if ((pid = fork()) == 0)
+	// {
+	// 	child(pipe_fd);
+	// }
+	// else
+	// {
+	// 	parent(pipe_fd);
+	// }
+	char	*args[] = {"ls", "-l", NULL};
+	execve("/bin/ls", args, NULL);
+	perror("execve");
+	exit(EXIT_FAILURE);
 	return (0);
 }
