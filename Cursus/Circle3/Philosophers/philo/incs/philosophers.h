da0842b167acbfb1291731821800e47d3b4663fa
Mori from 42 <64525549+angelidito@users.noreply.github.com>
2023-07-13 20:01:51 +0200

    jueves 13-7 philos

--- Cursus/Circle3/Philosophers/philo/incs/philosophers.h
+++ Cursus/Circle3/Philosophers/philo/incs/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: angmarti <angmarti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/19 20:04:28 by angmarti          #+#    #+#             */
-/*   Updated: 2023/06/19 20:11:08 by angmarti         ###   ########.fr       */
+/*   Updated: 2023/07/13 18:55:16 by angmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,4 +24,27 @@
 # include <sys/time.h> // gettimeofday
 # include <unistd.h>   // write y usleep
 
+typedef struct s_philo
+{
+	int				id;
+	int				is_eating;
+	int				times_eaten;
+	pthread_mutex_t	*left_fork;
+	pthread_mutex_t	*right_fork;
+	pthread_mutex_t	*print_mutex;
+	pthread_t		thread;
+}	t_philo;
+
+typedef struct s_table
+{
+	int				n_philos;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				n_times_must_eat;
+	int				philo_is_dead;
+	pthread_mutex_t	*forks;
+	pthread_mutex_t	print_mutex;
+	t_philo			*philos;
+}	t_table;
 #endif
--- Cursus/Circle3/Philosophers/philo/srcs/error.c
+++ Cursus/Circle3/Philosophers/philo/srcs/error.c
@@ -1,27 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   errors.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: angmarti <angmarti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/19 19:52:51 by angmarti          #+#    #+#             */
-/*   Updated: 2023/07/13 19:00:55 by angmarti         ###   ########.fr       */
+/*   Created: 2023/06/19 20:11:52 by angmarti          #+#    #+#             */
+/*   Updated: 2023/06/19 20:11:54 by angmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "../incs/philosophers.h"
-
-int main(int argc, char const *argv[])
-{
-	if (0 && argc != 5 && argc != 6)
-	{
-		printf("Error: wrong number of arguments\n");
-		return (1);
-	}
-	// pthread_mutex_t 
-		printf("Error: wrong number of arguments\n");
-	argv++;
-
-	return 0;
-}
