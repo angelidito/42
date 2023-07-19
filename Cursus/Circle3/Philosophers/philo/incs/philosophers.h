c62102f4a5ec94ef372f5cb824491fb434eccea8
Mori from 42 <64525549+angelidito@users.noreply.github.com>
2023-07-19 20:03:11 +0200

    .h update

--- Cursus/Circle3/Philosophers/philo/incs/philosophers.h
+++ Cursus/Circle3/Philosophers/philo/incs/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: angmarti <angmarti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/19 20:04:28 by angmarti          #+#    #+#             */
-/*   Updated: 2023/07/15 22:02:21 by angmarti         ###   ########.fr       */
+/*   Updated: 2023/07/19 16:43:19 by angmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,26 @@
 # define PHILOSOPHERS_H
 
 # include "colors.h"
-# include "functions.h"
+# include "structs.h" 
 # include "libs.h"
-# include "structs.h"
+// # include "functions.h"
+
+
+//	LIBFT
+
+char	*ft_itoa(int n);
+int		ft_atoi(const char *str);
+
+//	PHILOSOPHER
+
+void	*start(void *arg);
+
+//	TABLE
+
+int		init_table(t_table *table, int argc, char const *argv[]);
+
+//	TIME
+
+long	get_time(void);
 
 #endif
